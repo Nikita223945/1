e5a1b9767a447e44f1028091a8dc3869d1855685
Nikita223945 <nikpyat777@gmail.com>
2025-07-27 19:20:57 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+while(True):
+   words = {
+  "РОФЛ": "Шутка",
+  "ЩИЩ": "одобрение или восторг",
+  "КРИПОВЫЙ": " страшный, пугающий",
+  "АГРИТЬСЯ ": "злиться"
+  }
+word = input("Введите слово: ")
+word = word.upper()
+if word in words.keys():
+print(words[word])
+else:
+print("Слово не нашлось")
